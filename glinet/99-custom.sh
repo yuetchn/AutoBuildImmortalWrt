9df895b70ef8e178546d4bf8206fcf075906fbc9
YuetChn <yuetchn@163.com>
2025-11-03 20:12:46 +0800

    perf

--- glinet/99-custom.sh
+++ glinet/99-custom.sh
@@ -5,9 +5,9 @@ LOGFILE="/etc/config/uci-defaults-log.txt"
 uci set firewall.@zone[1].input='ACCEPT'
 
 # 设置主机名映射，解决安卓原生 TV 无法联网的问题
-uci add dhcp domain
-uci set "dhcp.@domain[-1].name=time.android.com"
-uci set "dhcp.@domain[-1].ip=203.107.6.88"
+# uci add dhcp domain
+# uci set "dhcp.@domain[-1].name=time.android.com"
+# uci set "dhcp.@domain[-1].ip=203.107.6.88"
 
 # 检查配置文件是否存在
 SETTINGS_FILE="/etc/config/pppoe-settings"
